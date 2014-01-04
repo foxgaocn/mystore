38e4e6504602ccf1e8450aa9504bcd4bf5ac6391
Haiyang (harry) Gao <foxgaocn@gmail.com>
2014-01-05 10:46:25 +1100

    change title and favicon

---
+++ app/overrides/admin_title.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(:virtual_path => "spree/admin/shared/_head",
+                     :name => "admin_title",
+                     :replace => "title",
+                     :text => "<title>Brew wizard admin</title>",
+                     :original => 'ff7679e31ae03c0a40431672bafff50857e930ba'
+                     )
